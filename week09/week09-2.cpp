b466528489d8bbedfa37d022b4f9584c24b0e3e4
王炫晟 <91250441+happy123903@users.noreply.github.com>
2022-04-22 12:00:43 +0800

    新增week09

---
+++ week09/week09-2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int a[10]={9,7,6,8,4,2,3,1,0,5};
+int main()
+{
+    int n=10;
+    for(int k=0; k<n-1; k++){
+        int change=0;
+        for(int i=0; i<n-1; i++){
+            if(a[i]<a[i+1]){
+                int t=a[i]; a[i]=a[i+1]; a[i+1]=t;
+                change++;
+            }
+        }
+        if(change==0) break;
+        for(int i=0; i<n; i++){
+            printf("%d ",a[i]);
+        }
+        printf("\n");
+    }
+
+}
+
